1f1ef81f6208a4e3e189503730c0978120edb8b6
Leo Mendoza <leo@okaythree.com>
2020-04-12 09:49:13 -0400

    Initialize repository

---
+++ lib/okaythree_palindrome.rb
@@ -0,0 +1,6 @@
+require "okaythree_palindrome/version"
+
+module OkaythreePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/okaythree_palindrome/version.rb
@@ -0,0 +1,3 @@
+module OkaythreePalindrome
+  VERSION = "0.1.0"
+end
